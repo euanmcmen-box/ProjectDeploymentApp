394f9139fa34e2daec6d381e39a30cfa8fa5c510
Euan McMenemin <euan.mcmenemin@webedia-group.com>
2023-02-05 10:42:39 +0000

    tokens

--- ProjectDeploymentApp/MainWindow.xaml.cs
+++ ProjectDeploymentApp/MainWindow.xaml.cs
@@ -1,8 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Configuration;
 using System.Diagnostics;
+using System.Linq;
 using System.Text;
 using System.Windows;
+// ReSharper disable InconsistentNaming
 
 namespace ProjectDeploymentApp;
 
@@ -11,17 +14,23 @@ namespace ProjectDeploymentApp;
 /// </summary>
 public partial class MainWindow : Window
 {
-    private const string GitRootUrl = "https://github.com/allocine/"; //"https://github.com/euanmcmen-box/"; 
+    private const string GitRootUrl = "https://github.com/euanmcmen-box/"; // "https://github.com/allocine/";
     
     public List<DeploymentApplication> DeploymentApplications { get; }
+
+    private bool previewPullRequests;
+
+    private string githubToken = default!;
 
     public MainWindow()
     {
         InitializeComponent();
 
+        ReadFromConfiguration();
+
         DeploymentApplications = new List<DeploymentApplication>()
         {
-            //new("Hello Planet", "HelloPlanet", "feature/use-venus", "master", "master", true),
+            new("Hello Planet", "HelloPlanet", "feature/use-venus", "master", "master", true),
             new("Banshee", "gla-Banshee", "dev", "uat", "main"),
             new("Boost", "gla-BoostTicketing", "dev", "uat", "master", false),
             new("Cyclops", "gla-Cyclops", string.Empty, "uat", "main"),
@@ -36,44 +45,81 @@ public partial class MainWindow : Window
         LbApplications.ItemsSource = DeploymentApplications;
     }
 
+    private void ReadFromConfiguration()
+    {
+        githubToken = ConfigurationManager.AppSettings["GITHUB_SSH_KEY"] ??
+                      throw new InvalidOperationException("GITHUB_SSH_KEY is not set in the application config.");
+
+        previewPullRequests = bool.TryParse(ConfigurationManager.AppSettings["PREVIEW_PULL_REQUESTS"], out var settingValue) && settingValue;
+    }
+
     private void BtnCreatePullRequests_OnClick(object sender, RoutedEventArgs e)
     {
         var deploymentLogSb = new StringBuilder();
         var selectedDeploymentEnvironmentTarget = (DeploymentEnvironmentTarget)CbDeploymentEnvironmentTarget.SelectedIndex;
 
-        foreach (var deploymentApplication in DeploymentApplications)
+        var selectedDeploymentApplications = DeploymentApplications
+            .Where(x => x.IsSelected)
+            .ToList();
+
+        foreach (var deploymentApplication in selectedDeploymentApplications)
         {
-            if (!deploymentApplication.IsSelected) continue;
+            var (outputLog, outputErrorLog) = SendPullRequestCommand(selectedDeploymentEnvironmentTarget, deploymentApplication);
 
-            var url = SendPullRequestCommand(selectedDeploymentEnvironmentTarget,
-                deploymentApplication);
-
-            deploymentLogSb.AppendLine($"{deploymentApplication.Name} - {url}");
+            deploymentLogSb.AppendLine(deploymentApplication.Name);
+            deploymentLogSb.AppendLine($"PR URL? - {outputLog}");
+            deploymentLogSb.AppendLine($"Errors? - {outputErrorLog}");
         }
 
         TbLog.Text = deploymentLogSb.ToString();
     }
 
-    private static string SendPullRequestCommand(DeploymentEnvironmentTarget target, DeploymentApplication application)
+    private (string, string) SendPullRequestCommand(DeploymentEnvironmentTarget target, DeploymentApplication application)
     {
+        var previewTextCommandSuffix = previewPullRequests ? "--web" : string.Empty;
+
         var commandText = target switch
         {
             DeploymentEnvironmentTarget.Uat =>
-                $"gh pr create --repo \"{GitRootUrl + application.RepositoryName}\" --head \"{application.DevBranchName}\" --base \"{application.UatBranchName}\" --title \"deploy to uat\" --body \"deploy to uat\"",
+                $"gh pr create --repo \"{GitRootUrl + application.RepositoryName}\" --head \"{application.DevBranchName}\" --base \"{application.UatBranchName}\" --title \"deploy to uat\" --body \"deploy to uat\" {previewTextCommandSuffix}",
             DeploymentEnvironmentTarget.Live =>
-                $"gh pr create --repo \"{GitRootUrl + application.RepositoryName}\" --head \"{application.UatBranchName}\" --base \"{application.LiveBranchName}\" --title \"deploy to live\" --body \"deploy to live\" --web",
+                $"gh pr create --repo \"{GitRootUrl + application.RepositoryName}\" --head \"{application.UatBranchName}\" --base \"{application.LiveBranchName}\" --title \"deploy to live\" --body \"deploy to live\" {previewTextCommandSuffix}",
             _ => throw new ArgumentOutOfRangeException(paramName: nameof(target))
         };
 
         var process = new Process();
-        process.StartInfo.FileName = "cmd.exe";
-        process.StartInfo.Arguments = $"/C {commandText}";
-        process.StartInfo.WindowStyle = ProcessWindowStyle.Hidden;
-        process.StartInfo.RedirectStandardOutput = true;
-        process.StartInfo.CreateNoWindow = true;
-        process.StartInfo.UseShellExecute = false;
+        process.StartInfo = GetStartInfoForCommand(commandText);
         process.Start();
         process.WaitForExit();
-        return process.StandardOutput.ReadToEnd();
+
+        var outputLog = process.StandardOutput.ReadToEnd();
+        var outputErrorLog = process.StandardError.ReadToEnd();
+
+        return (outputLog, outputErrorLog);
+    }
+
+    private ProcessStartInfo GetStartInfoForCommand(string commandText)
+    {
+        var processStartInfo = new ProcessStartInfo
+        {
+            FileName = "cmd.exe",
+            Arguments = $"/C {commandText}",
+            WindowStyle = ProcessWindowStyle.Hidden,
+            RedirectStandardOutput = true,
+            RedirectStandardError = true,
+            CreateNoWindow = true,
+            UseShellExecute = false
+        };
+
+        if (!EnvironmentVariablesContainsGithubAuthToken(processStartInfo))
+        {
+            processStartInfo.EnvironmentVariables.Add("GH_TOKEN", githubToken);
+        }
+
+        return processStartInfo;
+
+        static bool EnvironmentVariablesContainsGithubAuthToken(ProcessStartInfo startInfo) =>
+            startInfo.EnvironmentVariables.ContainsKey("GH_TOKEN") ||
+            startInfo.EnvironmentVariables.ContainsKey("GITHUB_TOKEN");
     }
 }
